a2a17f16ff7460e3624a0eb5cd003e40c11caf58
stevaras <steve_stizzy@yahoo.com>
2019-07-09 21:58:27 +0300

    First step. Folder Structure, Scss, Public

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ToggleButtonContainer } from './containers';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <ToggleButtonContainer />
+    </div>
+  );
+};
+
+export default App;
